921297d703c3b921764a85d936c67ce897ebce65
Stéphane HULARD <s.hulard@chstudio.fr>
2015-08-26 11:39:55 +0200

    Update connection contract
    Now fetch is sortable, countable and can be paginated

--- src/Connections/ConnectionInterface.php
+++ src/Connections/ConnectionInterface.php
@@ -23,46 +23,56 @@ interface ConnectionInterface
 {
     /**
      * Fetch some entities from a field value
-     * @param string $type Entity type to be manipulated
-     * @param string $term Term name to be processed
-     * @param mixed $value Searched term value
+     * @param  string  $type Entity type to be manipulated
+     * @param  string  $term Term name to be processed
+     * @param  mixed   $value Searched term value
+     * @param  integer $count Number of results to retrieve
+     * @param  integer $from Position of the first result to retrieve
+     * @param  array   $sort Sort definition as [field=>order]
      * @return array
      */
-    public function fetchBy($type, $term, $value);
+    public function fetchBy(
+        $type,
+        $term,
+        $value,
+        $count,
+        $from,
+        array $sort);
+
     /**
      * Count entity number matching criteria
-     * @param string $type Entity type to be manipulated
-     * @param string $term Term name to be processed
-     * @param mixed $value Searched term value
+     * @param  string $type Entity type to be manipulated
+     * @param  string $term Term name to be processed
+     * @param  mixed  $value Searched term value
      * @return integer
      */
     public function countBy($type, $term, $value);
 
     /**
      * Run a raw query against current connection
-     * @param string $type Entity type to be manipulated
-     * @param string $query
+     * @param  string $type Entity type to be manipulated
+     * @param  string $query
      * @return array
      */
     public function raw($type, $query);
 
     /**
      * Save the given entity
-     * @param Entity $entity
+     * @param  Entity $entity
      * @return boolean
      */
     public function save(Entity $entity);
 
     /**
      * Delete the given entity
-     * @param Entity $entity
+     * @param  Entity $entity
      * @return boolean
      */
     public function delete(Entity $entity);
 
     /**
      * Define how the connection will flush a transaction
-     * @param TransactionInterface $transaction
+     * @param  TransactionInterface $transaction
      * @return boolean
      */
     public function flush(TransactionInterface $transaction);
