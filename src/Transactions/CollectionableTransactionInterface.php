36b31fa7d403ccb35a7da444c67389b3e8937573
Stéphane HULARD <s.hulard@chstudio.fr>
2016-05-12 10:46:34 +0200

    Introduce a new contract to collect transactions
    Sometimes, we need to split transactions in chunks to allow handle more data. This interface allow to indentify that transactions.

---
+++ src/Transactions/CollectionableTransactionInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of the beebot package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Bee4 2015
+ * @author Stephane HULARD <s.hulard@chstudio.fr>
+ * @package BeeBot\Entity\Transactions
+ */
+
+namespace BeeBot\Entity\Transactions;
+
+use BeeBot\Entity\Entity;
+
+/**
+ * Allow to declare transaction types that are collectionable
+ * @package BeeBot\Entity\Transactions
+ */
+interface CollectionableTransactionInterface
+{
+}
